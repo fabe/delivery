6502fafd0c64417b7b1ccf05de6446dc78c04b50
Fabian Schultz <desk@fabianschultz.com>
2017-04-15 21:50:32 +0200

    Update delivery.

--- components/Delivery.js
+++ components/Delivery.js
@@ -6,22 +6,22 @@ import DeliveryItem from './DeliveryItem';
 @observer
 class Delivery extends React.Component {
   componentDidMount() {
-    // this.props.store.delivery.fetchDelivery(this.props.id);
+    if (!this.props.store.delivery.delivery) {
+      this.props.store.delivery.fetchDelivery(this.props.id);
+    }
   }
 
-  render() {
-    const { delivery } = this.props.store.delivery;
+  renderDelivery(delivery) {
     return (
-      <div>
+      <div className="container">
         <hgroup>
           <h1>{delivery.title}</h1>
           <p>{delivery.subtitle}</p>
         </hgroup>
+        <div className="divider" />
         {delivery.items.map((item, i) => <DeliveryItem key={i} item={item} />)}
         <style jsx>{`
           div {
-            max-width: 900px;
-            margin: 0 auto;
             text-align: center;
           }
 
@@ -33,17 +33,14 @@ class Delivery extends React.Component {
           hgroup p {
             margin-bottom: 0;
           }
-
-          hgroup:after {
-            display: inline-block;
-            width: 15%; height: 4px;
-            margin: 4rem auto 0;
-            background-color: #eee;
-            content: '';
-          }
         `}</style>
       </div>
     );
+  }
+
+  render() {
+    const { delivery } = this.props.store.delivery;
+    return delivery ? this.renderDelivery(delivery) : <div>Loading...</div>;
   }
 }
 
--- components/DeliveryItem.js
+++ components/DeliveryItem.js
@@ -1,17 +1,23 @@
 import React from 'react';
+import ImageZoom from 'react-medium-image-zoom';
 
 const DeliveryItem = ({ item }) => (
   <div>
-    <h2>{item.title}</h2>
+    <h2 className="item-title">{item.title}</h2>
     <p>{item.subtitle}</p>
-    <img src={item.image} alt={item.title} />
+    <ImageZoom
+      image={{
+        src: item.image,
+        alt: item.title,
+      }}
+      zoomImage={{
+        src: item.image,
+        alt: item.title,
+      }}
+    />
     <style jsx>{`
       div {
-        margin-bottom: 4em;
-      }
-
-      img {
-        max-width: 100%;
+        margin-bottom: 4rem;
       }
     `}</style>
   </div>
